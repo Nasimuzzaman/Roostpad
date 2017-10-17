07eb2234dcc1cf7e29b9abda581316057d33d08d
Nasimuzzaman <bsse0602@iit.du.ac.bd>
2017-10-17 13:28:50 +0600

    initialize setup page design

--- app/src/main/java/com/example/nasimuzzaman/roostpad/home/SetupActivity.java
+++ app/src/main/java/com/example/nasimuzzaman/roostpad/home/SetupActivity.java
@@ -2,6 +2,8 @@ package com.example.nasimuzzaman.roostpad.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.nasimuzzaman.roostpad.R;
 
@@ -11,5 +13,13 @@ public class SetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 }
