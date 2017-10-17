6cbdaac0522373bf6d336cc1cd37e64f1c2e3634
Nasimuzzaman <bsse0602@iit.du.ac.bd>
2017-10-17 17:17:46 +0600

    working on setup page

--- app/src/main/java/com/example/nasimuzzaman/roostpad/home/SetupActivity.java
+++ app/src/main/java/com/example/nasimuzzaman/roostpad/home/SetupActivity.java
@@ -1,45 +1,74 @@
 package com.example.nasimuzzaman.roostpad.home;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.nasimuzzaman.roostpad.R;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private Button users;
+    private Button contacts;
+    private Button calendarEvents;
+    private Button news;
+    private Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        users = (Button) findViewById(R.id.users);
+        contacts = (Button) findViewById(R.id.contacts);
+        calendarEvents = (Button) findViewById(R.id.calendar_events);
+        news = (Button) findViewById(R.id.news);
+        settings = (Button) findViewById(R.id.settings);
+
+        users.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    case R.id.action_users:
-                        Toast.makeText(SetupActivity.this, "User selected", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_contacts:
-                        Toast.makeText(SetupActivity.this, "Contact selected", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_settings:
-                        Toast.makeText(SetupActivity.this, "Setting selected", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-
-                return true;
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, UsersActivity.class);
+                startActivity(intent);
             }
         });
+
+        /*contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, ContactsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        calendarEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, CalendarEventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, NewsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
     }
 
     @Override
--- app/src/main/java/com/example/nasimuzzaman/roostpad/home/UsersActivity.java
+++ app/src/main/java/com/example/nasimuzzaman/roostpad/home/UsersActivity.java
@@ -2,6 +2,8 @@ package com.example.nasimuzzaman.roostpad.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Button;
 
 import com.example.nasimuzzaman.roostpad.R;
@@ -18,5 +20,13 @@ public class UsersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 }
